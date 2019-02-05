a5b612457d52cbdddf4603de0025ae70ee32e072
Joshua Carroll <joshc133@gmail.com>
2019-02-04 20:57:56 -0500

    Fixed preload script. Updated dark-mode to work on html class.

--- browser.js
+++ browser.js
@@ -1,6 +1,5 @@
 // Try triggering on a different event.
-alert('test');
 document.addEventListener('DOMContentLoaded', () => {
 	document.documentElement.classList.add(`os-${process.platform}`);
   document.documentElement.classList.add('dark-mode');
-};
+});
